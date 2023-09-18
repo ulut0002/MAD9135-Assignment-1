4ab488c0e721a9891a94a3a5e40c1a39580134bc
Serdar Ulutas <ulut0002@algonquinlive.com>
2023-09-18 10:54:17 -0400

    Boilerplate code for Screen1, 2 and 2

---
+++ screens/SecondPage.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function SecondPage() {
+  return (
+    <View>
+      <Text>HomePage</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default SecondPage;
---
+++ screens/ThirdPage.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function ThirdPage() {
+  return (
+    <View>
+      <Text>HomePage</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ThirdPage;
